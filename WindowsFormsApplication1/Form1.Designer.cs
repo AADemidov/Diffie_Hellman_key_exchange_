c6f1713493b6962d75cb120e434bff6c2ed395e4
Kosmos <Kosmos@KOSMOS->
2019-07-02 21:10:06 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RandomAandB = new System.Windows.Forms.Button();
+            this.RandomA = new System.Windows.Forms.TextBox();
+            this.RandomB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.RandomX = new System.Windows.Forms.TextBox();
+            this.RandomY = new System.Windows.Forms.TextBox();
+            this.X = new System.Windows.Forms.TextBox();
+            this.Y = new System.Windows.Forms.TextBox();
+            this.k1 = new System.Windows.Forms.TextBox();
+            this.k2 = new System.Windows.Forms.TextBox();
+            this.RandX = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.RandY = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.generiryemkey = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // RandomAandB
+            // 
+            this.RandomAandB.Location = new System.Drawing.Point(171, 25);
+            this.RandomAandB.Name = "RandomAandB";
+            this.RandomAandB.Size = new System.Drawing.Size(175, 58);
+            this.RandomAandB.TabIndex = 0;
+            this.RandomAandB.Text = "Рандом A и B";
+            this.RandomAandB.UseVisualStyleBackColor = true;
+            this.RandomAandB.Click += new System.EventHandler(this.RandomAandB_Click);
+            // 
+            // RandomA
+            // 
+            this.RandomA.Location = new System.Drawing.Point(99, 22);
+            this.RandomA.Name = "RandomA";
+            this.RandomA.Size = new System.Drawing.Size(66, 20);
+            this.RandomA.TabIndex = 1;
+            // 
+            // RandomB
+            // 
+            this.RandomB.Location = new System.Drawing.Point(99, 65);
+            this.RandomB.Name = "RandomB";
+            this.RandomB.Size = new System.Drawing.Size(66, 20);
+            this.RandomB.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "A =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "B =";
+            // 
+            // RandomX
+            // 
+            this.RandomX.Location = new System.Drawing.Point(47, 137);
+            this.RandomX.Name = "RandomX";
+            this.RandomX.Size = new System.Drawing.Size(66, 20);
+            this.RandomX.TabIndex = 5;
+            // 
+            // RandomY
+            // 
+            this.RandomY.Location = new System.Drawing.Point(250, 137);
+            this.RandomY.Name = "RandomY";
+            this.RandomY.Size = new System.Drawing.Size(66, 20);
+            this.RandomY.TabIndex = 6;
+            // 
+            // X
+            // 
+            this.X.Location = new System.Drawing.Point(47, 163);
+            this.X.Name = "X";
+            this.X.Size = new System.Drawing.Size(66, 20);
+            this.X.TabIndex = 7;
+            // 
+            // Y
+            // 
+            this.Y.Location = new System.Drawing.Point(250, 163);
+            this.Y.Name = "Y";
+            this.Y.Size = new System.Drawing.Size(66, 20);
+            this.Y.TabIndex = 8;
+            // 
+            // k1
+            // 
+            this.k1.Location = new System.Drawing.Point(47, 189);
+            this.k1.Name = "k1";
+            this.k1.Size = new System.Drawing.Size(66, 20);
+            this.k1.TabIndex = 9;
+            // 
+            // k2
+            // 
+            this.k2.Location = new System.Drawing.Point(250, 189);
+            this.k2.Name = "k2";
+            this.k2.Size = new System.Drawing.Size(66, 20);
+            this.k2.TabIndex = 10;
+            // 
+            // RandX
+            // 
+            this.RandX.Location = new System.Drawing.Point(119, 123);
+            this.RandX.Name = "RandX";
+            this.RandX.Size = new System.Drawing.Size(67, 34);
+            this.RandX.TabIndex = 11;
+            this.RandX.Text = "Рандом x";
+            this.RandX.UseVisualStyleBackColor = true;
+            this.RandX.Click += new System.EventHandler(this.RandX_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "x =";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "X =";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(0, 192);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Key 1 =";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(221, 166);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Y =";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(221, 141);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(21, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "y =";
+            // 
+            // RandY
+            // 
+            this.RandY.Location = new System.Drawing.Point(322, 123);
+            this.RandY.Name = "RandY";
+            this.RandY.Size = new System.Drawing.Size(66, 34);
+            this.RandY.TabIndex = 18;
+            this.RandY.Text = "Рандом y";
+            this.RandY.UseVisualStyleBackColor = true;
+            this.RandY.Click += new System.EventHandler(this.RandY_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(61, 111);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(38, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Алиса";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(267, 111);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(26, 13);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Боб";
+            // 
+            // generiryemkey
+            // 
+            this.generiryemkey.Location = new System.Drawing.Point(40, 224);
+            this.generiryemkey.Name = "generiryemkey";
+            this.generiryemkey.Size = new System.Drawing.Size(316, 44);
+            this.generiryemkey.TabIndex = 21;
+            this.generiryemkey.Text = "Генерировать ключ";
+            this.generiryemkey.UseVisualStyleBackColor = true;
+            this.generiryemkey.Click += new System.EventHandler(this.generiryemkey_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(119, 166);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(67, 13);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "= B^x mod A";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(119, 192);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(67, 13);
+            this.label12.TabIndex = 23;
+            this.label12.Text = "= Y^x mod A";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(321, 192);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(67, 13);
+            this.label13.TabIndex = 25;
+            this.label13.Text = "= X^y mod A";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(321, 166);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(67, 13);
+            this.label14.TabIndex = 24;
+            this.label14.Text = "= B^y mod A";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(200, 192);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "Key 2 =";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 280);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.generiryemkey);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.RandY);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.RandX);
+            this.Controls.Add(this.k2);
+            this.Controls.Add(this.k1);
+            this.Controls.Add(this.Y);
+            this.Controls.Add(this.X);
+            this.Controls.Add(this.RandomY);
+            this.Controls.Add(this.RandomX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RandomB);
+            this.Controls.Add(this.RandomA);
+            this.Controls.Add(this.RandomAandB);
+            this.Name = "Form1";
+            this.Text = "Ключевой обмен Диффи-Хеллмана";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RandomAandB;
+        private System.Windows.Forms.TextBox RandomA;
+        private System.Windows.Forms.TextBox RandomB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox RandomX;
+        private System.Windows.Forms.TextBox RandomY;
+        private System.Windows.Forms.TextBox X;
+        private System.Windows.Forms.TextBox Y;
+        private System.Windows.Forms.TextBox k1;
+        private System.Windows.Forms.TextBox k2;
+        private System.Windows.Forms.Button RandX;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button RandY;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button generiryemkey;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+        double chisloA, chisloB, chisloX, chisloY, stepenbA, stepenbB;
+        string Key1, Key2;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RandomAandB_Click(object sender, EventArgs e) // Генерируем случайно число А и В
+        {
+            RandomA.Text = (rnd.Next(100)).ToString();
+            chisloA = Convert.ToDouble(RandomA.Text);
+            RandomB.Text = (rnd.Next(100)).ToString();
+            chisloB = Convert.ToDouble(RandomB.Text);
+        }
+
+        private void RandX_Click(object sender, EventArgs e) // Генерируем случайно число Х
+        {
+            RandomX.Text = (rnd.Next(10)).ToString();
+            chisloX = Convert.ToDouble(RandomX.Text);
+        }
+
+        private void RandY_Click(object sender, EventArgs e) // Генерируем случайно число У
+        {
+            RandomY.Text = (rnd.Next(10)).ToString();
+            chisloY = Convert.ToDouble(RandomY.Text);
+        }
+
+        private void generiryemkey_Click(object sender, EventArgs e)
+        {
+            if (RandomA.Text == "" || RandomB.Text == "" || RandomX.Text == "" || RandomY.Text == "") //проверка текстбоксов на пустоту
+            {
+                MessageBox.Show("Сгенерируйте все переменные");
+            }
+
+            stepenbA = (Math.Pow(chisloB, chisloX) % chisloA); // В возводим в степень х и мод А
+            stepenbB = (Math.Pow(chisloB, chisloY) % chisloA); // В возводим в степень у и мод А
+
+            X.Text = Convert.ToString(stepenbA % chisloA); // Вывовдим в текст бокс Х
+            Y.Text = Convert.ToString(stepenbB % chisloA); // Вывовдим в текст бокс У
+            
+
+            Key1 = Convert.ToString(((Math.Pow(stepenbA, chisloY) % chisloA))); // степень А возводим в степень у и мод А
+            Key2 = Convert.ToString(((Math.Pow(stepenbB, chisloX) % chisloA))); //степень В возводим в степень х и мод А
+
+            k1.Text = Convert.ToString(Key1); //Вывовдим в текст бокс к1
+            k2.Text = Convert.ToString(Key2); //Вывовдим в текст бокс к2
+
+        }
+    }
+}
